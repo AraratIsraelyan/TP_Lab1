b6d899fb1dace3e4d2e8005edb25653ce48a6119
AraratIsraelyan <35899855+AraratIsraelyan@users.noreply.github.com>
2019-02-21 14:42:14 +0300

    Лабораторная 1
    Файлы проекта.

---
+++ Project1.dpr
@@ -0,0 +1,17 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+uses    	{список модулей, с которыми устанавливается связь}
+  SysUtils;  	{Системные утилиты}
+
+begin		{Начало программы}
+  Writeln('Hello, World from Delphi!');
+  Writeln('This is the console application');
+  Writeln; 	 {Процедуры Write и WriteLn выполняют
+             вывод информации в устройство стандартного вывода}
+  Writeln('Press <Enter> to quit.');
+  Readln; 		{Процедуры Read и ReadLn выполняют чтение информации
+              из устройства стандартного ввода.}
+
+end. 		{Конец программы}
